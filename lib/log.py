6c0044ead9eeaf04ad502c044ace3ac6f9d82fbc
Mathias Jeschke <mjeschke@juniper.net>
2023-09-08 13:38:19 +0200

    Added file logging when --debug is enabled

--- lib/log.py
+++ lib/log.py
@@ -1,13 +1,28 @@
+from datetime import datetime
 import logging
+import os
 import sys
 
 FORMAT = '%(asctime)s | %(levelname)-7s | %(message)s'
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
-def set_debug():
+def set_debug(app='python3-logging'):
     for handler in logging.root.handlers[:]:
         logging.root.removeHandler(handler)
     logging.basicConfig(format=FORMAT, level=logging.DEBUG, datefmt=DATE_FORMAT)
+
+    # when debug is enabled, also log into a file
+    try:
+        os.mkdir(f'/tmp/{app}')
+    except FileExistsError:
+        pass
+    try:
+        log_file = '{:%Y-%m-%d_%H-%M-%S}.log'.format(datetime.now())
+        file_handler = logging.FileHandler(f'/tmp/{app}/{log_file}')
+        file_handler.setFormatter(logging.Formatter(FORMAT))
+        logging.getLogger().addHandler(file_handler)
+    except:
+        pass
 
 def format_msg(*msg):
     return ' '.join([str(s) for s in [*msg]])
